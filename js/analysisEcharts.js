fdd795333fdb623dc956245823b2eae65913870a
ac383640116 <383640116@qq.com>
2016-07-09 16:11:37 +0800

    能耗图表门禁详情

--- js/analysisEcharts.js
+++ js/analysisEcharts.js
@@ -6,33 +6,15 @@ var getOption = function(chartType) {
 		legend: {
 			data: ['故障统计']
 		},
+		color:['#007aff'],//设置曲线颜色
 		grid: {
 			x: 35,
 			x2: 10,
 			y: 30,
 			y2: 25
 		},
-		toolbox: {
-			show: false,
-			feature: {
-				mark: {
-					show: true
-				},
-				dataView: {
-					show: true,
-					readOnly: false
-				},
-				magicType: {
-					show: true,
-					type: ['line', 'bar']
-				},
-				restore: {
-					show: true
-				},
-				saveAsImage: {
-					show: true
-				}
-			}
+		tooltip : {    //	气泡提示框
+			trigger: 'axis' //直角坐标系中的一个坐标轴，坐标轴可分为类目型、数值型或时间型
 		},
 		calculable: false,
 		xAxis: [{
@@ -48,6 +30,7 @@ var getOption = function(chartType) {
 		series: [{
 			name: '故障统计',
 			type: chartType,
+			itemStyle: {normal: {areaStyle: {type: 'default'}}},//图表样式设置
 			data: [31, 35, 24, 26, 50, 75, 65, 56, 43, 27, 20, 28]
 		}]
 	};
@@ -112,6 +95,7 @@ var getOption1= function(chartType) {
 		series: [{
 			name: '耗电量统计',
 			type: chartType,
+			itemStyle: {normal: {areaStyle: {type: 'default'}}},//图表样式设置
 			data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
 		}]
 	};
