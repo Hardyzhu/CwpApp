f588be315a74052759b4500c69df51f41ac909d3
yinxia <xia.yin@winphone.us>
2016-07-09 16:34:46 +0800

    图表修改

--- js/analysisEcharts.js
+++ js/analysisEcharts.js
@@ -1,39 +1,39 @@
 ﻿
 var getOption = function(chartType) {
-	var chartOption = chartType == 'pie' ? {
-		calculable: false,
-	} : {
+	var chartOption= {
 		legend: {
 			data: ['故障统计']
 		},
 		color:['#007aff'],//设置曲线颜色
 		grid: {
-			x: 35,
-			x2: 10,
-			y: 30,
-			y2: 25
+			x: 60,
+			x2: 30,
+			y: 35,
+			y2: 30
 		},
 		tooltip : {    //	气泡提示框
 			trigger: 'axis' //直角坐标系中的一个坐标轴，坐标轴可分为类目型、数值型或时间型
 		},
-		calculable: false,
+		calculable: true,
 		xAxis: [{
 			type: 'category',
-			data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+			boundaryGap : false,
+			data: ['1月', '2月', '3月', '4月', '5月', '6月']
 		}],
 		yAxis: [{
 			type: 'value',
-			splitArea: {
-				show: true
+			axisLabel : {//设置纵坐标的单位
+				formatter: '{value} kwh'
 			}
 		}],
 		series: [{
 			name: '故障统计',
-			type: chartType,
+			type:'line',
+			smooth:true,
 			itemStyle: {normal: {areaStyle: {type: 'default'}}},//图表样式设置
-			data: [31, 35, 24, 26, 50, 75, 65, 56, 43, 27, 20, 28]
+			data: [31, 35, 24, 26, 50, 75]
 		}]
-	};
+	}
 	return chartOption;
 };
 var byId = function(id) {
@@ -47,58 +47,40 @@ lineChart.setOption(getOption('line'));
 <!--第二个；列表展示-->
 
 var getOption1= function(chartType) {
-	var chartOption = chartType== 'pie' ? {
-		calculable: false,
-	} : {
+	var chartOption ={
 		legend: {
 			data: ['耗电量统计']
 		},
+		color:['#007aff'],//设置曲线颜色
 		grid: {
-			x: 35,
-			x2: 10,
-			y: 30,
-			y2: 25
+			x: 60,
+			x2: 30,
+			y: 35,
+			y2: 30
 		},
-		toolbox: {
-			show: false,
-			feature: {
-				mark: {
-					show: true
-				},
-				dataView: {
-					show: true,
-					readOnly: false
-				},
-				magicType: {
-					show: true,
-					type: ['line', 'bar']
-				},
-				restore: {
-					show: true
-				},
-				saveAsImage: {
-					show: true
-				}
-			}
+		tooltip : {    //	气泡提示框
+			trigger: 'axis' //直角坐标系中的一个坐标轴，坐标轴可分为类目型、数值型或时间型
 		},
-		calculable: false,
+		calculable: true,
 		xAxis: [{
 			type: 'category',
-			data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+			boundaryGap : false,
+			data: ['1月', '2月', '3月', '4月', '5月', '6月']
 		}],
 		yAxis: [{
 			type: 'value',
-			splitArea: {
-				show: true
+			axisLabel : {//设置纵坐标的单位
+				formatter: '{value} kwh'
 			}
 		}],
 		series: [{
 			name: '耗电量统计',
-			type: chartType,
+			type:'line',
+			smooth:true,
 			itemStyle: {normal: {areaStyle: {type: 'default'}}},//图表样式设置
-			data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+			data: [80, 35, 67, 26, 77, 45]
 		}]
-	};
+	}
 	return chartOption;
 };
 var byId = function(id) {
