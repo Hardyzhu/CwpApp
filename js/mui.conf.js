d63410b458e2e1e3e50509c4d7829151dfecef6e
yeshengqiang <673089899@qq.com>
2016-08-03 21:17:49 +0800

    引导页调整

--- js/mui.conf.js
+++ js/mui.conf.js
@@ -10,6 +10,6 @@
 //本地IP
 //var global_url="http://192.168.92.201:28090";
 //阿里云IP
-//var global_url="http://58.96.169.201:28090";
+var global_url="http://58.96.169.201:28090";
 //中移IP
-var global_url="http://192.168.100.65:20010";
+//var global_url="http://192.168.100.65:20010";
